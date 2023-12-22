9fd0d2329ae8635fa2c33ead13e524ed92874b55
yeszvq <mipalasy@gmail.com>
2023-12-22 22:51:30 +0300

    Добавлены новые объекты, перестроена комната алтаря, исправлена проблема со светом
    - Добавлены: алтарь, гильотина, так же перестроена комната алтаря после зала
    - Решена проблема, когда игрок преграждал собой весь свет, необходимо ставить источника света на определённые точки

---
+++ scripts/tilemap/item_5.gd
@@ -0,0 +1,13 @@
+extends TileMap
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func open_door():
+	set_cellv(Vector2(31,-75),-1)
+	set_cellv(Vector2(32,-75),-1)
+	set_cellv(Vector2(33,-75),-1)
+	set_cellv(Vector2(31,-75),112)
+	set_cellv(Vector2(31,-76),113)
+	pass
